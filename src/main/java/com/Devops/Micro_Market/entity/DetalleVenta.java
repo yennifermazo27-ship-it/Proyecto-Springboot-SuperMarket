474c941f7a1a5a03a1ce945b02b81ca80d2ccc1c
yennifer mazo montoya <yennifermzo27@gmail.com>
2026-05-27 20:05:53 -0500

    Se crea entidad DetalleVenta

---
+++ src/main/java/com/Devops/Micro_Market/entity/DetalleVenta.java
@@ -0,0 +1,40 @@
+package com.Devops.Micro_Market.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalle_venta")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleVenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venta_id")
+    private Venta venta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario", precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal subtotal;
+}
